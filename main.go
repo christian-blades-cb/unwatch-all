c89230ba0d5b15d8c108031b4dde1e78c09cbc62
Christian Blades <cblades@pindrop.com>
2016-12-05 22:21:08 -0500

    doesn't work because oauth?

--- main.go
+++ main.go
@@ -5,17 +5,19 @@ import (
 	"github.com/jessevdk/go-flags"
 	"github.com/uber-go/zap"
 	"golang.org/x/oauth2"
+	"net/url"
 	"os"
 )
 
 var opts struct {
-	Token    string `short:"t" long:"token" env:"GITHUB_TOKEN" description:"github oauth token" required:"true"`
-	Simulate bool   `short:"s" long:"simulate" description:"don't make any subscription changes"`
+	Token     string `short:"t" long:"token" env:"GITHUB_TOKEN" description:"github oauth token" required:"true"`
+	Simulate  bool   `short:"s" long:"simulate" description:"don't make any subscription changes"`
+	GithubURL string `long:"hub-url" env:"GITHUB_URL" description:"github url (such as your enterprise github url)" default:"https://api.github.com"`
 }
 
 var logger zap.Logger
 
-func main() {
+func init() {
 	logger = zap.New(zap.NewTextEncoder())
 
 	_, err := flags.Parse(&opts)
@@ -23,17 +25,19 @@ func main() {
 		logger.Error("unable to parse flags", zap.Error(err))
 		os.Exit(1)
 	}
+}
 
-	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: opts.Token})
-	tc := oauth2.NewClient(oauth2.NoContext, ts)
+func main() {
+	client := getGithubClient()
+	unsubscribeLoop(client)
+}
 
-	client := github.NewClient(tc)
-
+func unsubscribeLoop(client *github.Client) {
 	listOpts := github.ListOptions{
 		PerPage: 50,
 	}
 	for {
-		repos, resp, err := client.Activity.ListWatched("", nil)
+		repos, resp, err := client.Activity.ListWatched("", &listOpts)
 		if err != nil {
 			logger.Error("exception while listing watch repos", zap.Error(err))
 			os.Exit(1)
@@ -45,28 +49,35 @@ func main() {
 		}
 		listOpts.Page = resp.NextPage
 	}
+}
 
+func getGithubClient() *github.Client {
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: opts.Token})
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
+
+	client := github.NewClient(tc)
+	baseUrl, err := url.Parse(opts.GithubURL)
+	if err != nil {
+		logger.Error("unable to parse provided url", zap.String("github url", opts.GithubURL), zap.Error(err))
+		os.Exit(1)
+	}
+
+	client.BaseURL = baseUrl
+	return client
 }
 
 func unsubscribeFromRepos(client *github.Client, repos []*github.Repository) {
-	subscription := &github.Subscription{
-		Subscribed: flase(),
-	}
-
 	for _, repo := range repos {
-		logger.Info("unsubscribing", zap.Bool("simulation", opts.Simulate), zap.String("owner", repo.Owner.String()), zap.String("repo", *repo.Name))
+		ownerStr := repo.Owner.Login
+		repoStr := repo.Name
+		unsubLogger := logger.With(zap.Bool("simulation", opts.Simulate), zap.String("owner", *ownerStr), zap.String("repo", *repoStr))
+		unsubLogger.Info("unsubscribing")
 		if !opts.Simulate {
-			client.Activity.SetRepositorySubscription(repo.Owner.String(), repo.String(), subscription)
+			_, err := client.Activity.DeleteRepositorySubscription(repo.Owner.String(), repo.String())
+			if err != nil {
+				unsubLogger.Error("error unsubscribing", zap.Error(err))
+				os.Exit(1)
+			}
 		}
 	}
-}
-
-func flase() *bool {
-	x := false
-	return &x
-}
-
-func tru() *bool {
-	x := true
-	return &x
 }
